465d56b5ac09392efde49a4bbef743b601d6fcf8
colspan <collin@collindigm.com>
2024-04-08 15:10:46 +0300

    Provided a Text Argument to the Argument chain

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
     let _matches = App::new("echor")
         .version("0.1.0")
         .author("Collo Collins <collo@fastmail.com>")
         .about("Rust echo")
+        .arg(
+            Arg::with_name("text")
+            .value_name("TEXT")
+            .help("Input text")
+            .required(true)
+            .min_values(1)
+        )
         .get_matches();
 }
