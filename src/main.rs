b5503bb07d5492b2dfe4ee831c57b9b89125f1ca
colspan <collin@collindigm.com>
2024-04-08 15:17:34 +0300

    omit newline argument added

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{App, Arg};
 
 fn main() {
-    let _matches = App::new("echor")
+    let matches = App::new("echor")
         .version("0.1.0")
         .author("Collo Collins <collo@fastmail.com>")
         .about("Rust echo")
@@ -12,5 +12,13 @@ fn main() {
             .required(true)
             .min_values(1)
         )
+        .arg(
+            Arg::with_name("omit_newline")
+            .short("n")
+            .help("Do not print newline")
+            .takes_value(false)
+        )
         .get_matches();
+
+    println!("{:#?}", matches);
 }
