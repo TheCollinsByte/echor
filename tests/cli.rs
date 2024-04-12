22317d902c7fc4b9300a3b94b075287eabe95b19
colspan <collin@collindigm.com>
2024-04-12 12:53:46 +0300

    Update the Unit Test to make them safe and clean

--- tests/cli.rs
+++ tests/cli.rs
@@ -3,27 +3,30 @@ use std::fs;
 use assert_cmd::Command;
 use predicates::prelude::predicate;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]
-fn run() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn run() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("hello")
         .assert()
         .success();
+    Ok(())
 }
 
 #[test]
-fn hello1() {
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn hello1() -> TestResult {
+    let expected = fs::read_to_string("tests/expected/hello1.txt")?;
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
